da0c9f75cf58a5a1519dd8eb1723ab5862d928e1
=Far2Deluxe <=fares.boo@gmail.com>
2025-09-17 01:28:39 +0300

    Modified save location of model training

--- src/data/YoloV8TrainerCUDA_V2.py
+++ src/data/YoloV8TrainerCUDA_V2.py
@@ -3,8 +3,12 @@ import torch
 from torch.optim import AdamW
 import ultralytics
 from ultralytics import YOLO
+from pathlib import Path
 
 
+current_dir = Path(__file__).parent
+project_dir = current_dir.parent.parent
+yolo_models_dir = project_dir / "src" / "data"
 
 
 def main():
@@ -16,7 +20,7 @@ def main():
         print("GPU Name:", torch.cuda.get_device_name(0))
 
     # Load YOLO model
-    model = YOLO("yolov8n.pt")
+    model = YOLO(project_dir / "src" / "data" / "yolov8s.pt")
 
     # Train on GPU (device=0). Reduce workers to avoid multiprocessing issues.
     model.train(
@@ -31,6 +35,8 @@ def main():
         patience=15,         # Early stopping patience
         imgsz=640,           # Image size
         batch=16,            # Batch size
+        project = project_dir / "src" / "models" ,
+        name = "Whiteboard Model",            
         save=True,           # Save checkpoints
         plots=True           # Generate training plots
     )
